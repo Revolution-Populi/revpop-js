54a8c9ea2458301442c71c8e14ef7850f879e077
Revolution Populi <robert.b.rosenthal@gmail.com>
2021-04-12 12:33:23 +0300

    Use single crypto library

--- test/storage/CloudStorage.js
+++ test/storage/CloudStorage.js
@@ -1,4 +1,5 @@
 import assert from "assert";
+import secureRandom from "secure-random";
 import {CloudStorage, MemoryAdapter, PrivateKey} from "../../lib";
 
 describe("CloudStorage", () => {
@@ -24,6 +25,19 @@ describe("CloudStorage", () => {
             const id = await storage.crypto_save_buffer(test_data, pk, pk2.toPublicKey());
             const load_data = await storage.crypto_load_buffer(id, pk.toPublicKey(), pk2);
             assert.notStrictEqual(load_data, test_data);
+        });
+    });
+
+    describe("crypto_load_buffer", function() {
+        it ("correct load previously saved content using the symmetric key", async function() {
+            const storage = new CloudStorage(new MemoryAdapter());
+            const test_data = Buffer.from('test data for a single key');
+            const symmetric_key = secureRandom.randomBuffer(32).toString('hex');
+            const content_key = {algo: 'aes-256-cbc', key: symmetric_key, iv: secureRandom.randomBuffer(16).toString('hex')};
+            const id = await storage.crypto_save_content(test_data, content_key);
+            const load_data = await storage.crypto_load_content(id, content_key);
+            assert.notStrictEqual(load_data, null);
+            assert.strictEqual(load_data.toString('hex'), test_data.toString('hex'));
         });
     });
 });
