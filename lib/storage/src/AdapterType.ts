638ff520d29093e8ade30d71f3eaca2632305d69
trafterian <99242625+Trafterian@users.noreply.github.com>
2022-03-09 20:59:10 +0300

    s3 node adapter

--- lib/storage/src/AdapterType.ts
+++ lib/storage/src/AdapterType.ts
@@ -4,7 +4,8 @@ export enum AdapterType {
     IPFS = 'ipfs',
     GOOGLE_DRIVE_WEB = 'google_drive_web',
     GOOGLE_DRIVE_NODE = 'google_drive_node',
-    AMAZON_S3_WEB = 'amazon_s3_web'
+    AMAZON_S3_WEB = 'amazon_s3_web',
+    AMAZON_S3_NODE = 'amazon_s3_node'
 }
 
 export function webFromStorage(storageType: StorageType): AdapterType {
